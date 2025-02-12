051448d3b488aee4e23fdc522cd3e8fe127402e1
Alva studio <alvaofichial@gmail.com>
2025-02-12 21:22:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "ya apa ga ? ",
+    "Boong aja",
+    "serus lagi",
+    " please...",
+    "Pikir lagi sebelum.nyesel",
+    "Nangis nih guwaa",
+    "Beneran inii",
+    "I will be very very very sad...",
+    "Dahlah ngambek guaa",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
